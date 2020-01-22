d96395503da8d399cbabafff9411b70dc322b61c
conbec <56614504+conbec@users.noreply.github.com>
2020-01-22 14:08:02 -0800

    sort of working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import moment from 'moment';
 
 function App() {
+  const [weekState, setWeekState] = useState([]);
+  let start = useState(moment().startOf('week'));
+
+  const getWeek = (dir) => {
+    const week = [];
+    let i = 0;
+
+
+    while (week.length < 7) {
+      if (dir === 1) {
+        week.push(moment(start).add(i++, 'day'));
+        start = moment(start).add(i++, 'day');
+      }
+      else {
+        week.push(moment(start).subtract(i++, 'day'));
+        start = moment(start).subtract(i++, 'day');
+      }
+    }
+    setWeekState([...week])
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {weekState.map((item, idx) => {
+          return (
+            <li key={idx}>{item.format('MMM Do YYYY')}</li>
+          )
+        })}
+      </ul>
+      <button onClick={() => getWeek(1)}>Get Week</button>
+      <button onClick={() => getWeek(0)}>Get Week</button>
     </div>
   );
 }
